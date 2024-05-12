619a1059515d0572f4e458f6ef0782fc247b5712
sebika nepal <72345247+sebika123@users.noreply.github.com>
2024-05-12 15:53:52 +0545

    backend flask connected

---
+++ language/app.py
@@ -0,0 +1,45 @@
+# from flask import Flask, request, jsonify
+# from translation import translate_to_nepali
+
+# app = Flask(__name__)
+
+# @app.route('/translate', methods=['POST'])
+# def translate():
+#     english_text = request.json.get('text')
+#     nepali_translation = translate_to_nepali(english_text)
+#     return jsonify({'translation': nepali_translation})
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+  
+# from flask import Flask, request, jsonify
+# from translation import translate_to_nepali
+
+# app = Flask(__name__)
+
+# @app.route('/translate', methods=['POST'])
+# def translate():
+#     english_text = request.json.get('text')
+#     target_language = request.json.get('target')
+#     nepali_translation = translate_to_nepali(english_text)
+#     return jsonify({'translation': nepali_translation})
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from translation import translate_to_nepali
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
+@app.route('/translate', methods=['POST'])
+def translate():
+    english_text = request.json.get('text')
+    target_language = request.json.get('target')
+    nepali_translation = translate_to_nepali(english_text)
+    return jsonify({'translation': nepali_translation})
+
+if __name__ == '__main__':
+    app.run(debug=True)
